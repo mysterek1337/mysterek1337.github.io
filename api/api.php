0d65f4ea2fb4899bc0d831770e8ad8a17496af31
mysterek_ <59736397+mysterek1337@users.noreply.github.com>
2022-09-03 22:25:00 +0200

    Update api.php

--- api/api.php
+++ api/api.php
@@ -1,1 +1,34 @@
-ayo
+<?php
+
+function getUserIP()
+{
+    // Get real visitor IP behind CloudFlare network
+    // StackOverflow: https://stackoverflow.com/questions/13646690/how-to-get-real-ip-from-visitor
+    if (isset($_SERVER["HTTP_CF_CONNECTING_IP"])) {
+              $_SERVER['REMOTE_ADDR'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
+              $_SERVER['HTTP_CLIENT_IP'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
+    }
+    $client  = @$_SERVER['HTTP_CLIENT_IP'];
+    $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+    $remote  = $_SERVER['REMOTE_ADDR'];
+
+    if(filter_var($client, FILTER_VALIDATE_IP))
+    {
+        $ip = $client;
+    }
+    elseif(filter_var($forward, FILTER_VALIDATE_IP))
+    {
+        $ip = $forward;
+    }
+    else
+    {
+        $ip = $remote;
+    }
+
+    return $ip;
+}
+
+
+$user_ip = getUserIP();
+
+echo $user_ip; ?>
